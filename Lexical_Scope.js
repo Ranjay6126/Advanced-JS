59df0ed3685cd91f10f20d61712ed0747d6c262e
Ranjay6126 <panditranjay33@gmail.com>
2025-11-14 18:46:20 +0530

    Advanced js in to Deep

---
+++ Lexical_Scope.js
@@ -0,0 +1,17 @@
+// var a;  
+// Console.log(a);
+
+// If( a === undefined){
+// Console.log("a is undefined");
+// } else {
+// Console.log(" a  is not define");}
+
+function a(){
+    c();
+
+    function c(){
+         console.log(b);
+    }
+}
+var b=10;
+a();
